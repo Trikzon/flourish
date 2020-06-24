6b6806e20a27b7ba5550c4a3ed0beb5f650dfbb2
Trikzon <trikzondev@gmail.com>
2020-06-24 00:17:27 -0400

    Add License

--- src/main/java/com/trikzon/flora_doubling/mixin/DispenserBlockAccessor.java
+++ src/main/java/com/trikzon/flora_doubling/mixin/DispenserBlockAccessor.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright 2020 Trikzon
+ *
+ * Flora Doubling is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * https://www.gnu.org/licenses/
+ */
 package com.trikzon.flora_doubling.mixin;
 
 import net.minecraft.block.DispenserBlock;
