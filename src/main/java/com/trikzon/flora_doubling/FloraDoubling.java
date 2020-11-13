ed02dd6472a28e60a6149ab1c46105b7c9dbc321
Trikzon <trikzondev@gmail.com>
2020-11-13 12:21:20 -0500

    Add particles to dispenser behavior

--- src/main/java/com/trikzon/flora_doubling/FloraDoubling.java
+++ src/main/java/com/trikzon/flora_doubling/FloraDoubling.java
@@ -40,13 +40,6 @@ public class FloraDoubling implements ModInitializer {
 
     public static Config.ConfigBean CONFIG = new Config.ConfigBean();
 
-    public static boolean isTargetFlower(Block block) {
-        return ((block.getClass() != WitherRoseBlock.class) || CONFIG.allowWitherRoses) &&
-                (DOUBLING_FLORA_TAG.contains(block) || CONFIG.doublingFlora.contains(getId(block.asItem())) ||
-                (SMALL_FLOWERS_TAG.contains(block) && CONFIG.useSmallFlowersTag) ||
-                (TALL_FLOWERS_TAG.contains(block) && CONFIG.useTallFlowersTag));
-    }
-
     @Override
     public void onInitialize() {
         if (MOD_CONFIG_FILE.exists()) {
@@ -63,7 +56,7 @@ public class FloraDoubling implements ModInitializer {
     }
 
     private ActionResult onUseBlock(PlayerEntity player, World world, Hand hand, BlockHitResult result) {
-        boolean success = grow(player.getStackInHand(hand), world, result.getBlockPos(), true);
+        boolean success = grow(player.getStackInHand(hand), world, result.getBlockPos());
         if (!success) return ActionResult.PASS;
 
         if (!player.isCreative()) {
@@ -71,17 +64,15 @@ public class FloraDoubling implements ModInitializer {
         }
         return ActionResult.SUCCESS;
     }
-  
-    private boolean grow(ItemStack stack, World world, BlockPos pos, boolean particlesOnClient) {
+
+    private boolean grow(ItemStack stack, World world, BlockPos pos) {
         if (!(stack.getItem() instanceof BoneMealItem)) return false;
         Block block = world.getBlockState(pos).getBlock();
         if (!isTargetFlower(block))
             return false;
 
-        if (world.isClient && particlesOnClient) {
+        if (world.isClient) {
             BoneMealItem.createParticles(world, pos, world.random.nextInt(14));
-        } else if (!world.isClient && !particlesOnClient) {
-            createParticles((ServerWorld)world, pos, world.random.nextInt(14));
         }
         Block.dropStack(world, pos, new ItemStack(block, 1));
 
@@ -92,46 +83,10 @@ public class FloraDoubling implements ModInitializer {
         return Registry.ITEM.getId(item).toString();
     }
 
-    // Copied from BoneMealItem but edited to create the particles from the server instead of the client.
-    // This is so that dispensers will emit particles when growing flowers.
-    private void createParticles(ServerWorld world, BlockPos pos, int count) {
-        Random random = world.random;
-        BlockState blockState = world.getBlockState(pos);
-
-        if (!blockState.isAir()) {
-            if (count == 0) count = 15;
-            double d = 0.5D;
-            double g;
-            if (blockState.isOf(Blocks.WATER)) {
-                count *= 3;
-                g = 1.0D;
-                d = 3.0D;
-            } else if (blockState.isOpaqueFullCube(world, pos)) {
-                pos = pos.up();
-                count *= 4;
-                d = 3.0D;
-                g = 1.0D;
-            } else {
-                g = blockState.getOutlineShape(world, pos).getMax(Direction.Axis.Y);
-            }
-            world.spawnParticles(
-                    ParticleTypes.HAPPY_VILLAGER,
-                    (double)pos.getX() + 0.5D, (double)pos.getY() + 0.5D, (double)pos.getZ() + 0.5D,
-                    1, 0.0D, 0.0D, 0.0D, 1.0
-            );
-
-            for (int i = 0; i < count; ++i) {
-                double h = random.nextGaussian() * 0.02D;
-                double j = random.nextGaussian() * 0.02D;
-                double k = random.nextGaussian() * 0.02D;
-                double l = 0.5D - d;
-                double m = (double)pos.getX() + l + random.nextDouble() * d * 2.0D;
-                double n = (double)pos.getY() + random.nextDouble() * g;
-                double o = (double)pos.getZ() + l + random.nextDouble() * d * 2.0D;
-                if (!world.getBlockState((new BlockPos(m, n, o)).down()).isAir()) {
-                    world.spawnParticles(ParticleTypes.HAPPY_VILLAGER, m, n, o, 1, h, j, k, 1.0);
-                }
-            }
-        }
+    public static boolean isTargetFlower(Block block) {
+        return ((block.getClass() != WitherRoseBlock.class) || CONFIG.allowWitherRoses) &&
+                (DOUBLING_FLORA_TAG.contains(block) || CONFIG.doublingFlora.contains(getId(block.asItem())) ||
+                        (SMALL_FLOWERS_TAG.contains(block) && CONFIG.useSmallFlowersTag) ||
+                        (TALL_FLOWERS_TAG.contains(block) && CONFIG.useTallFlowersTag));
     }
 }
