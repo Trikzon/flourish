306c5c1df3216dd77145dcd5cb4b78bb2eb685e1
Trikzon <trikzondev@gmail.com>
2020-11-14 01:33:00 -0500

    Fix overwriting default bonemeal behavior and infinite bonemeal

--- src/main/java/com/trikzon/flora_doubling/dispenser/BoneMealDispenserBehavior.java
+++ src/main/java/com/trikzon/flora_doubling/dispenser/BoneMealDispenserBehavior.java
@@ -6,7 +6,9 @@ import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.dispenser.DispenserBehavior;
+import net.minecraft.block.dispenser.FallibleItemDispenserBehavior;
 import net.minecraft.block.dispenser.ItemDispenserBehavior;
+import net.minecraft.item.BoneMealItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.ParticleTypes;
 import net.minecraft.server.world.ServerWorld;
@@ -19,7 +21,21 @@ import java.util.Random;
 
 public class BoneMealDispenserBehavior implements DispenserBehavior {
 
-    private static class Default extends ItemDispenserBehavior {
+    // Copied from DispenserBehavior#registerDefaults
+    private static class Default extends FallibleItemDispenserBehavior {
+        @Override
+        protected ItemStack dispenseSilently(BlockPointer pointer, ItemStack stack) {
+            this.setSuccess(true);
+            World world = pointer.getWorld();
+            BlockPos blockPos = pointer.getBlockPos().offset(pointer.getBlockState().get(DispenserBlock.FACING));
+            if (!BoneMealItem.useOnFertilizable(stack, world, blockPos) && !BoneMealItem.useOnGround(stack, world, blockPos, (Direction)null)) {
+                this.setSuccess(false);
+            } else if (!world.isClient) {
+                world.syncWorldEvent(2005, blockPos, 0);
+            }
+
+            return stack;
+        }
     }
 
     private static class Modified extends ItemDispenserBehavior {
@@ -29,10 +45,9 @@ public class BoneMealDispenserBehavior implements DispenserBehavior {
             World world = pointer.getWorld();
             Block block = world.getBlockState(pos).getBlock();
 
-            if (FloraDoubling.isTargetFlower(block) && !world.isClient) {
-                Block.dropStack(world, pos, new ItemStack(block, 1));
-                createParticles((ServerWorld) world, pos, world.random.nextInt(14));
-            }
+            Block.dropStack(world, pos, new ItemStack(block, 1));
+            createParticles((ServerWorld) world, pos, world.random.nextInt(14));
+            stack.decrement(1);
             return stack;
         }
 
@@ -82,7 +97,11 @@ public class BoneMealDispenserBehavior implements DispenserBehavior {
 
     @Override
     public ItemStack dispense(BlockPointer pointer, ItemStack stack) {
-        if (FloraDoubling.CONFIG.dispenser) {
+        BlockPos pos = pointer.getBlockPos().offset(pointer.getBlockState().get(DispenserBlock.FACING));
+        World world = pointer.getWorld();
+        Block block = world.getBlockState(pos).getBlock();
+
+        if (FloraDoubling.CONFIG.dispenser && FloraDoubling.isTargetFlower(block) && !world.isClient) {
             return new Modified().dispense(pointer, stack);
         } else {
             return new Default().dispense(pointer, stack);
