1e0105328bc6351388c0bc43908620954ccb57e4
Trikzon <trikzondev@gmail.com>
2020-11-14 00:01:14 -0500

    1.3.0: Use vanilla tags for config

--- src/main/java/com/trikzon/flora_doubling/FloraDoubling.java
+++ src/main/java/com/trikzon/flora_doubling/FloraDoubling.java
@@ -1,15 +1,15 @@
 package com.trikzon.flora_doubling;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.WitherRoseBlock;
 import net.minecraft.item.BoneMealItem;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ITag;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.common.util.FakePlayer;
 import net.minecraftforge.event.entity.player.BonemealEvent;
 import net.minecraftforge.eventbus.api.Event;
 import net.minecraftforge.fml.common.Mod;
@@ -25,6 +25,8 @@ public class FloraDoubling {
 
     public static final File MOD_CONFIG_FILE = new File("./config/" + MOD_ID + ".json");
     public static final ITag.INamedTag<Block> DOUBLING_FLORA_TAG = BlockTags.makeWrapperTag(MOD_ID + ":doubling_flora");
+    public static final ITag.INamedTag<Block> SMALL_FLOWERS_TAG = BlockTags.SMALL_FLOWERS;
+    public static final ITag.INamedTag<Block> TALL_FLOWERS_TAG = BlockTags.TALL_FLOWERS;
 
     public static Config.ConfigBean CONFIG = new Config.ConfigBean();
 
@@ -35,24 +37,33 @@ public class FloraDoubling {
                 CONFIG = config;
             }
         } else {
-            Config.write(CONFIG, true);
+            Config.write(CONFIG);
         }
 
         MinecraftForge.EVENT_BUS.addListener(this::onBoneMeal);
     }
 
     private void onBoneMeal(BonemealEvent event) {
-        boolean success = grow(event.getStack(), event.getWorld(), event.getPos());
-        if (success) event.setResult(Event.Result.ALLOW);
+        if (!(!CONFIG.dispenser && event.getPlayer() instanceof FakePlayer)) {
+            boolean success = grow(event.getStack(), event.getWorld(), event.getPos());
+            if (success) event.setResult(Event.Result.ALLOW);
+        }
     }
 
     private boolean grow(ItemStack stack, World world, BlockPos pos) {
         if (!(stack.getItem() instanceof BoneMealItem)) return false;
         Block block = world.getBlockState(pos).getBlock();
 
-        boolean success = DOUBLING_FLORA_TAG.contains(block) || CONFIG.doublingFlora.contains(block.getRegistryName().toString());
+        boolean success = isTargetFlower(block);
         if (success) Block.spawnAsEntity(world, pos, new ItemStack(block, 1));
 
         return success;
+    }
+
+    public static boolean isTargetFlower(Block block) {
+        return ((block.getClass() != WitherRoseBlock.class) || CONFIG.allowWitherRoses) &&
+                (DOUBLING_FLORA_TAG.contains(block) || CONFIG.doublingFlora.contains(block.getRegistryName().toString())) ||
+                (SMALL_FLOWERS_TAG.contains(block) && CONFIG.useSmallFlowersTag) ||
+                (TALL_FLOWERS_TAG.contains(block) && CONFIG.useTallFlowersTag);
     }
 }
