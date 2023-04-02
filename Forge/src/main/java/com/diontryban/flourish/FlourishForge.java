f703177fb0b001feb5e4e17446c0ed091d427a5c
Dion Tryban <github@diontryban.com>
2023-04-02 01:51:02 -0400

    Add license header and fix mixins at runtime

--- Forge/src/main/java/com/diontryban/flourish/FlourishForge.java
+++ Forge/src/main/java/com/diontryban/flourish/FlourishForge.java
@@ -1,13 +1,36 @@
+/*
+ * This file is part of Flourish.
+ * A copy of this program can be found at https://github.com/Trikzon/flourish.
+ * Copyright (C) 2023 Dion Tryban
+ *
+ * Flourish is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Flourish is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Flourish. If not, see <https://www.gnu.org/licenses/>.
+ */
+
 package com.diontryban.flourish;
 
+import com.diontryban.ash.api.modloader.forge.ForgeModLoader;
 import com.diontryban.flourish.client.FlourishClientForge;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.fml.DistExecutor;
+import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 @Mod(Flourish.MOD_ID)
 public class FlourishForge {
     public FlourishForge() {
+        ForgeModLoader.registerMod(Flourish.MOD_ID, ModLoadingContext.get(), FMLJavaModLoadingContext.get());
         Flourish.init();
         DistExecutor.safeCallWhenOn(Dist.CLIENT, () -> FlourishClientForge::new);
     }
--- Forge/src/main/java/com/diontryban/flourish/client/FlourishClientForge.java
+++ Forge/src/main/java/com/diontryban/flourish/client/FlourishClientForge.java
@@ -1,12 +1,26 @@
-package com.diontryban.flourish.client;
+/*
+ * This file is part of Flourish.
+ * A copy of this program can be found at https://github.com/Trikzon/flourish.
+ * Copyright (C) 2023 Dion Tryban
+ *
+ * Flourish is free software: you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or (at your
+ * option) any later version.
+ *
+ * Flourish is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with Flourish. If not, see <https://www.gnu.org/licenses/>.
+ */
 
-import com.diontryban.ash.api.modloader.forge.ForgeModLoader;
-import com.diontryban.flourish.Flourish;
-import net.minecraftforge.fml.ModLoadingContext;
+package com.diontryban.flourish.client;
 
 public class FlourishClientForge {
     public FlourishClientForge() {
-        ForgeModLoader.registerMod(Flourish.MOD_ID, ModLoadingContext.get());
         FlourishClient.init();
     }
 }
